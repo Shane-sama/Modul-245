79946593a0b31360d5af2d1c5efad32880cfc639
Shane-sama <shane.roellin@icloud.com>
2024-03-19 16:55:03 +0100

    Project and Structure

---
+++ Workhelper/Workhelper/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Workhelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetQuestion();
+        }
+
+        public static string question = "";
+
+        public static void GetQuestion()
+        {
+            Console.WriteLine("Please enter your Text");
+            question = Console.ReadLine();
+        }
+
+        public static void CreatePrompt()
+        {
+
+        }
+
+        public static void AskAI()
+        {
+
+        }
+
+        public static void GetRespone()
+        {
+
+        }
+
+        public static void ShowResponse()
+        {
+
+        }
+    }
+}
